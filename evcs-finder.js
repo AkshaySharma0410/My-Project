74fd86822f29a686b57997be632a47f8d3f61ff9
AkshaySharma0410 <akshaysharmawork2@gmail.com>
2025-01-15 23:00:09 +0530

    Add files via upload

---
+++ evcs-finder.js
@@ -0,0 +1,45 @@
+var countryCode = "AU";
+// var selectedLocation = {};
+
+var map;
+
+// lat and lng values will be altered with dynamic variables returned from the selected station in the result section "#search-results"
+// or it can be directly received as an object with "lat" and "lng" and "zoom" properties.
+
+var locationResult = { lat: -37.877721, lng: 145.04558 };
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: locationResult,
+    zoom: 15
+  });
+  console.log("initializing");
+}
+
+$(document).on("click", ".selectStationBtn", function(event) {
+  var resultLat = Number($(this).data("lat"));
+  var resultLng = Number($(this).data("long"));
+  locationResult.lat = resultLat;
+  locationResult.lng = resultLng;
+
+  initMap();
+  setMarker();
+});
+
+function firstResult(firstLat, firstLon) {
+  locationResult.lat = firstLat;
+  locationResult.lng = firstLon;
+
+  initMap();
+  setMarker();
+}
+
+// Sets the marker on the map based on the selected location in results area
+function setMarker() {
+  // creates the marker on the map
+  var marker = new google.maps.Marker({
+    position: locationResult,
+    map: map,
+    title: "EVCS Finder"
+  });
+}
